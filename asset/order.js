a0b091f29004c0b27634b547584540acf7c5b370
Osondu Favour <86885410+Alphaeinsteinium@users.noreply.github.com>
2024-12-10 19:02:47 -0800

    Add files via upload

---
+++ asset/order.js
@@ -0,0 +1,36 @@
+const Orders = [
+{
+    productName: 'Foldable mini drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+
+{
+    productName: 'Foldable mini drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+
+{
+    productName: 'Foldable mini drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+
+{
+    productName: 'Foldable mini drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+
+{
+    productName: 'Foldable mini drone',
+    productNumber: '85631',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+},
+]
